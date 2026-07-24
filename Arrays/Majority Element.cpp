a420c2c3f1c48768922806b63367d45be7b3dbae
Shivam Varshney <ultimateshivam5@gmail.com>
2026-07-24 15:33:57 +0530

    Majority Element.cpp

---
+++ Arrays/Majority Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> freq;
+        for(int i=0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(freq[nums[i]]>nums.size()/2) return nums[i];
+        }
+        return 0;
+    }
+};
